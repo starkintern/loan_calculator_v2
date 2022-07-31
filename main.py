998779ac86ba3c307a8d75ae70aa2b55505c0c41
jacob <jsov886@gmail.com>
2022-07-31 17:30:56 -0400

    Added the match statements for usr_period_input data

--- main.py
+++ main.py
@@ -12,11 +12,21 @@ if __name__ == '__main__':
 
     while "no" not in usr_input.lower():
         usr_principal = int(input("Please enter your total loan amount: "))
-        usr_interest_rate = int(input("Please enter your loan interest rate as a percentage: "))
-        usr_period = int(input("Please enter your interest period frequency per year (365, 12, 6, 1): "))
+        usr_interest_rate = float(input("Please enter your loan interest rate as a percentage: "))
+        usr_period_input = int(input("Please enter your interest period frequency per year (365, 12, 6, 1): "))
         usr_num_periods = int(input("Please enter how many years you have accrued interest for: "))
 
-        _loan_list.append(loan(usr_principal, usr_interest_rate, usr_period, usr_num_periods))
+        _loan_list.append(loan(usr_principal, usr_interest_rate, usr_period_input, usr_num_periods))
+
+        match usr_period_input.lower:
+            case "day" | "days" | "365":
+                usr_period_input = 365
+            case "month" | "months" | "12":
+                usr_period_input = 12
+            case "semi-annually" | "bi-annually" | "6":
+                usr_period_input = 6
+            case "year" | "years" | "1":
+                usr_period_input = 1
 
         usr_input = input("Would you like to enter another loan? ")
 
