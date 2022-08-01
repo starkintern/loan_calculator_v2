5fabfaecfd3c1683fc802c88f7e4906530aa4514
jacob <jsov886@gmail.com>
2022-08-01 16:27:02 -0400

    I removed the "self" argument/parameter from the loan function, given that I do not need "self" in a function unless it's inside a class. That also fixed my issue with usr_num_periods being "unfilled".

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ if __name__ == '__main__':
 
     usr_input = input("Would you like to enter a loan?")
 
-    def loan(self, principal, interest_rate, period, num_periods):
-        self.principal = principal
-        self.interest_rate = interest_rate
-        self.period = period
-        self.num_periods = num_periods
+    def loan(principal, interest_rate, period, num_periods):
+        principal = principal
+        interest_rate = interest_rate
+        period = period
+        num_periods = num_periods
 
     while "no" not in usr_input.lower():
         usr_principal = int(input("Please enter your total loan amount: "))
         usr_interest_rate = float(input("Please enter your loan interest rate as a percentage: "))
         usr_period_input = (input("Please enter your interest period frequency per year (365, 12, 6, 1): "))
-        usr_num_periods = int(input("Please enter how many years you have accrued interest for: "))
+        usr_num_periods = (input("Please enter how many years you have accrued interest for: "))
 
         _loan_list.append(loan(usr_principal, usr_interest_rate, usr_period_input, usr_num_periods))
 
